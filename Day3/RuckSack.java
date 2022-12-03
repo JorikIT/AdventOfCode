8314b5c3df694785fce97e91a7165672ad8251e9
Jorik Barten <jorik.bartem@outlook.com>
2022-12-03 23:37:51 +0100

    Day 3 solution part two

--- Day3/RuckSack.java
+++ Day3/RuckSack.java
@@ -3,31 +3,56 @@ package Day3;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class RuckSack {
 
     public static void main(String[] args) throws IOException {
-        AtomicInteger score = new AtomicInteger();
+        AtomicInteger prioritiesPartOne = new AtomicInteger();
         List<Character> alphabet = "abcdefghijklmnopqrstuvwxyz".chars().mapToObj(c -> (char) c).toList();
         Files.readAllLines(Paths.get("Day3/input.txt")).forEach(line -> {
             String compartmentsOne = line.substring(0, line.length() / 2);
             String compartmentsTwo = line.substring(line.length() / 2);
-            score.addAndGet(sumPriorities(compartmentsOne, compartmentsTwo, alphabet));
+            prioritiesPartOne.addAndGet(sumPriorities(compartmentsOne, compartmentsTwo, alphabet));
         });
+
+        final AtomicInteger counter = new AtomicInteger();
+        AtomicInteger prioritiesPartTwo = new AtomicInteger();
+        final Collection<List<String>> groups = Files.readAllLines(Paths.get("Day3/input.txt"))
+                .stream()
+                .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / 3))
+                .values();
+        groups.forEach(group -> prioritiesPartTwo.addAndGet(sumPrioritiesPartTwo(group.get(0), group.get(1), group.get(2), alphabet)));
+        System.out.println(prioritiesPartTwo);
     }
 
     private static int sumPriorities(String compartmentsOne, String compartmentsTwo, List<Character> alphabet) {
-        AtomicInteger score = new AtomicInteger();
+        AtomicInteger sumPriorities = new AtomicInteger();
         List<Character> charsCompartmentsOne = compartmentsOne.chars().mapToObj(c -> (char) c).toList();
         List<Character> charsCompartmentsTwo = compartmentsTwo.chars().mapToObj(c -> (char) c).toList();
 
         charsCompartmentsOne.forEach(letter -> {
             if (charsCompartmentsTwo.contains(letter)) {
-                score.set(Character.isLowerCase(letter) ? (alphabet.indexOf(letter) + 1) : (alphabet.indexOf(Character.toLowerCase(letter)) + 27));
+                sumPriorities.set(Character.isLowerCase(letter) ? (alphabet.indexOf(letter) + 1) : (alphabet.indexOf(Character.toLowerCase(letter)) + 27));
             }
         });
-        return score.get();
+        return sumPriorities.get();
+    }
+
+    private static int sumPrioritiesPartTwo(String ruckSackOne, String ruckSackTwo, String ruckSackThree, List<Character> alphabet) {
+        AtomicInteger sum = new AtomicInteger();
+        List<Character> charsRuckSackOne = ruckSackOne.chars().mapToObj(c -> (char) c).toList();
+        List<Character> charsRuckSackTwo = ruckSackTwo.chars().mapToObj(c -> (char) c).toList();
+        List<Character> charsRuckSackThree = ruckSackThree.chars().mapToObj(c -> (char) c).toList();
+
+        charsRuckSackOne.forEach(letter -> {
+            if (charsRuckSackTwo.contains(letter) && charsRuckSackThree.contains(letter)) {
+                sum.set(Character.isLowerCase(letter) ? (alphabet.indexOf(letter) + 1) : (alphabet.indexOf(Character.toLowerCase(letter)) + 27));
+            }
+        });
+        return sum.get();
     }
 }
