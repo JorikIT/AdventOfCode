35901995f9381c635218fd36b8d06dc3db5b7a88
Jorik Barten <jorik.bartem@outlook.com>
2022-12-04 21:23:07 +0100

    Day 4 part two

--- Day4/CampCleanup.java
+++ Day4/CampCleanup.java
@@ -12,20 +12,33 @@ public class CampCleanup {
     public static void main(String[] args) throws IOException {
 
         AtomicInteger sum = new AtomicInteger();
+        AtomicInteger sum2 = new AtomicInteger();
         Files.readAllLines(Paths.get("Day4/input.txt")).forEach(line -> {
-            List<String> assignments = Arrays.stream(line.split(",")).toList();
-            String assignmentElfOne = assignments.get(0);
-            String assignmentElfTwo = assignments.get(1);
-            int minRangeOne = Integer.parseInt(assignmentElfOne.split("-")[0]);
-            int maxRangeOne = Integer.parseInt(assignmentElfOne.split("-")[1]);
-            int minRangeTwo = Integer.parseInt(assignmentElfTwo.split("-")[0]);
-            int maxRangeTwo = Integer.parseInt(assignmentElfTwo.split("-")[1]);
+            count(sum, line, "one");
+            count(sum2, line, "two");
+        });
+    }
 
+    private static void count(AtomicInteger sum, String line, String part) {
+        List<String> assignments = Arrays.stream(line.split(",")).toList();
+        String assignmentElfOne = assignments.get(0);
+        String assignmentElfTwo = assignments.get(1);
+        int minRangeOne = Integer.parseInt(assignmentElfOne.split("-")[0]);
+        int maxRangeOne = Integer.parseInt(assignmentElfOne.split("-")[1]);
+        int minRangeTwo = Integer.parseInt(assignmentElfTwo.split("-")[0]);
+        int maxRangeTwo = Integer.parseInt(assignmentElfTwo.split("-")[1]);
+
+        if ("one".equals(part)) {
             if (minRangeOne <= minRangeTwo && maxRangeOne >= maxRangeTwo || minRangeTwo <= minRangeOne && maxRangeTwo >= maxRangeOne) {
                 sum.getAndIncrement();
             }
-        });
+        } else {
+            if (minRangeOne >= minRangeTwo && minRangeOne <= maxRangeTwo
+                    || maxRangeOne >= minRangeTwo && maxRangeOne <= maxRangeTwo
+                    || minRangeTwo >= minRangeOne && minRangeTwo <= maxRangeOne
+                    || maxRangeTwo >= minRangeOne && maxRangeTwo <= maxRangeOne) {
+                sum.getAndIncrement();
+            }
+        }
     }
 }
-
-
