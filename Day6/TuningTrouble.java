134a5afba09b6b49ca64f374668c19180585f4a5
Jorik Barten <jorik.bartem@outlook.com>
2022-12-07 18:29:14 +0100

    simplyfied

--- Day6/TuningTrouble.java
+++ Day6/TuningTrouble.java
@@ -1,28 +1,15 @@
 package Day6;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class TuningTrouble {
 
-    public static final int DEFAULT_BUFFER_SIZE = 8192;
-
     public static void main(String[] args) throws IOException {
 
-        InputStream input = Files.newInputStream(Path.of("Day6/input.txt"), new StandardOpenOption[]{StandardOpenOption.READ});
-
-        ByteArrayOutputStream result = new ByteArrayOutputStream();
-        byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
-        int length;
-        while ((length = input.read(buffer)) != -1) {
-            result.write(buffer, 0, length);
-        }
-        List<Character> characters = result.toString().chars().mapToObj(c -> (char) c).toList();
+        List<Character> characters = Files.readString(Path.of("Day6/input.txt")).chars().mapToObj(c -> (char) c).toList();
         tune(characters, 3);
         tune(characters, 13);
     }
@@ -38,7 +25,6 @@ public class TuningTrouble {
     }
 
     private static boolean unique(List<Character> characters) {
-        int size = characters.size();
-        return characters.stream().distinct().toList().size() == size;
+        return characters.size() == characters.stream().distinct().toList().size();
     }
 }
